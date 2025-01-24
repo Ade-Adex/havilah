9d4738d5ea79761b2a3cd5f7a82e3ea05366dc8a
AdeAdex <adeoluamole@gmail.com>
2025-01-24 18:39:56 +0100

    gallery images added and map in gallery page

--- app/(routes)/gallery/page.tsx
+++ app/(routes)/gallery/page.tsx
@@ -1,16 +1,70 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 import galleryBg from "@/public/images/gallery-bg.png";
 import PagesHero from "@/app/components/PagesHero";
+import Heading from "@/app/components/Heading";
+import Line from "@/app/components/Line";
+import { galleryImages } from "@/app/data/galleryImages";
+import Image from "next/image";
 
 const GalleryPage = () => {
   const pathname = usePathname();
   const formattedPathname = pathname.replace("/", "");
+  const [selectedCategory, setSelectedCategory] = useState("event"); // Default category
+
   return (
     <main>
       <PagesHero bgImage={galleryBg} pageTitle={formattedPathname} />
+      <div className="py-[75px] px-4 md:px-12 lg:px-16 xl:px-24">
+        <Heading
+          text="Take a glimpse of our stunning"
+          text2="hall and its exceptional features"
+          className="justify-start"
+        />
+        <Line ClassName="justify-center" />
+
+        {/* Category buttons */}
+        <div className="flex gap-4 mb-8 justify-center mt-8">
+          {galleryImages.map((category, index) => (
+            <button
+              key={index}
+              onClick={() => setSelectedCategory(category.category)}
+              className={`px-4 py-2 text-[16px] font-[500] capitalize font-robotoSlab leading-[21.1px] ${
+                selectedCategory === category.category
+                  ? " text-havilah-whiskey underline"
+                  : "text-havilah-deep-cove"
+              }`}
+            >
+              {category.category}
+            </button>
+          ))}
+        </div>
+
+        {/* Display images for the selected category */}
+        {galleryImages
+          .filter((category) => category.category === selectedCategory)
+          .map((filteredCategory) => (
+            <div key={filteredCategory.category}>
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+                {filteredCategory.images.map((image, imgIndex) => (
+                  <div
+                    key={imgIndex}
+                    className="relative w-full h-0 pb-[75%] overflow-hidden"
+                  >
+                    <Image
+                      src={image}
+                      alt={`${filteredCategory.category} Image ${imgIndex + 1}`}
+                      fill
+                      className="object-cover w-full"
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+      </div>
     </main>
   );
 };
---
+++ app/data/galleryImages.ts
@@ -0,0 +1,58 @@
+import GalleryImage1 from "@/public/images/gallery/gallery-event-1.png";
+import GalleryImage2 from "@/public/images/gallery/gallery-event-2.png";
+import GalleryImage3 from "@/public/images/gallery/gallery-event-3.png";
+import GalleryImage4 from "@/public/images/gallery/gallery-event-4.png";
+import GalleryImage5 from "@/public/images/gallery/gallery-event-5.png";
+import GalleryImage6 from "@/public/images/gallery/gallery-event-6.png";
+import GalleryImage7 from "@/public/images/gallery/gallery-event-7.png";
+import GalleryImage8 from "@/public/images/gallery/gallery-event-8.png";
+import GalleryImage9 from "@/public/images/gallery/gallery-event-9.png";
+import GalleryImage10 from "@/public/images/gallery/gallery-event-10.png";
+import GalleryImage11 from "@/public/images/gallery/gallery-event-11.png";
+import GalleryImage12 from "@/public/images/gallery/gallery-event-12.png";
+import GalleryImage13 from "@/public/images/gallery/gallery-event-13.png";
+import GalleryImage14 from "@/public/images/gallery/gallery-event-14.png";
+import GalleryImage15 from "@/public/images/gallery/gallery-event-15.png";
+import GalleryImage16 from "@/public/images/gallery/gallery-event-16.png";
+
+export const galleryImages = [
+  {
+    category: "event",
+    images: [
+      GalleryImage1,
+      GalleryImage2,
+      GalleryImage3,
+      GalleryImage4,
+      GalleryImage5,
+      GalleryImage6,
+      GalleryImage7,
+      GalleryImage8,
+      GalleryImage9,
+      GalleryImage10,
+      GalleryImage11,
+      GalleryImage12,
+      GalleryImage13,
+      GalleryImage14,
+      GalleryImage15,
+      GalleryImage16,
+    ],
+  },
+  {
+    category: "hall",
+    images: [
+      GalleryImage1, // Add appropriate hall images here
+      GalleryImage3, // You can adjust these to match actual images for "hall"
+      GalleryImage5,
+      GalleryImage7,
+    ],
+  },
+  {
+    category: "video",
+    images: [
+      GalleryImage2, // Add appropriate video images here
+      GalleryImage4,
+      GalleryImage6,
+      GalleryImage8,
+    ],
+  },
+];
