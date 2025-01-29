77378ed44f1bdaf4072781bd37fe09b959af15ba
AdeAdex <adeoluamole@gmail.com>
2025-01-29 21:14:34 +0100

    new updates

---
+++ app/(routes)/gallery/MediaModal.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import ReactPlayer from "react-player";
+import Image from "next/image";
+import { IoClose } from "react-icons/io5";
+
+type MediaModalProps = {
+  media: any;
+  closeModal: () => void;
+};
+
+const MediaModal = ({ media, closeModal }: MediaModalProps) => {
+  const isVideo = media.type === "video";
+  const imageSrc = media.src;
+  const imageAlt = media.description || "Media image";
+
+  return (
+    <div
+      className="fixed inset-0 bg-black bg-opacity-60 flex justify-center items-center z-50"
+      onClick={closeModal}
+    >
+      <div className="relative max-w-4xl max-h-[80vh] p-4 bg-white rounded-lg overflow-hidden">
+        <button
+          onClick={closeModal}
+          className="absolute top-2 right-2 bg-red-600 text-white p-2 rounded-full z-10"
+        >
+          <IoClose />
+        </button>
+        {/* Handle media type: Video or Image */}
+        {isVideo ? (
+          <ReactPlayer
+            url={media.src}
+            playing
+            controls
+            width="100%"
+            height="100%"
+          />
+        ) : (
+          <div className="relative w-full h-full max-h-[60vh] overflow-hidden">
+            <Image
+              src={imageSrc}
+              alt={imageAlt}
+              layout="intrinsic"
+              width={600}  // Set a width that works well for your design
+              height={400} // Adjust the height accordingly to maintain aspect ratio
+              className="object-contain" // Ensure the image doesn't overflow
+              priority
+            />
+          </div>
+        )}
+        <p className="mt-4 text-lg">{media.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default MediaModal;
--- app/(routes)/gallery/page.tsx
+++ app/(routes)/gallery/page.tsx
@@ -1,9 +1,200 @@
-"use client";
+// "use client";
 
+// import { usePathname } from "next/navigation";
+// import React, { useState, useEffect } from "react";
+// import ReactPlayer from "react-player";
+// import Image from "next/image";
+// import galleryBg from "@/public/images/gallery-bg.png";
+// import PagesHero from "@/app/components/PagesHero";
+// import Heading from "@/app/components/Heading";
+// import Line from "@/app/components/Line";
+// import { getGalleryData } from "@/app/cache/useGalleryCache";
+// import { Gallery } from "@/app/types/gallery";
+// import useInView from "@/app/hooks/useInView";
+// import GallerySkeleton from "./GallerySkeleton";
+// import { FaPlay } from "react-icons/fa";
+
+// const GalleryPage = () => {
+//   const pathname = usePathname();
+//   const formattedPathname = pathname.replace("/", "").toUpperCase();
+//   const [selectedCategory, setSelectedCategory] = useState("event");
+//   const [galleryImages, setGalleryImages] = useState<Gallery[]>([]);
+//   const [isLoading, setIsLoading] = useState(true);
+
+//   useEffect(() => {
+//     // Fetch gallery data with caching
+//     const fetchData = async () => {
+//       try {
+//         const data = await getGalleryData();
+//         setGalleryImages(data);
+//       } catch (error) {
+//         console.error("Error fetching gallery data:", error);
+//       } finally {
+//         setIsLoading(false); // Stop loading after data fetch
+//       }
+//     };
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <main>
+//       <PagesHero bgImage={galleryBg} pageTitle={formattedPathname} />
+//       <div className="py-[75px] px-4 md:px-12 lg:px-16 xl:px-24">
+//         <Heading
+//           text="Take a glimpse of our stunning"
+//           text2="hall and its exceptional features"
+//           className="justify-start"
+//         />
+//         <Line ClassName="justify-center" />
+
+//         {/* Category buttons */}
+//         <div className="flex gap-4 mb-8 justify-center mt-8">
+//           {galleryImages.map((category, index) => (
+//             <button
+//               key={index}
+//               onClick={() => setSelectedCategory(category.category)}
+//               className={`px-4 py-2 text-[16px] font-[500] capitalize font-robotoSlab leading-[21.1px] ${
+//                 selectedCategory === category.category
+//                   ? "text-havilah-whiskey underline"
+//                   : "text-havilah-deep-cove"
+//               }`}
+//             >
+//               {category.category}
+//             </button>
+//           ))}
+//         </div>
+
+//         {/* Display media or skeleton for the selected category */}
+//         {isLoading ? (
+//           <GallerySkeleton />
+//         ) : (
+//           galleryImages
+//             .filter((category) => category.category === selectedCategory)
+//             .map((filteredCategory) => (
+//               <div key={filteredCategory.category}>
+//                 <div
+//                   className={`grid grid-cols-2 sm:grid-cols-2 ${
+//                     filteredCategory.images.some((media) =>
+//                       media.src.asset.url.endsWith(".mp4")
+//                     )
+//                       ? "md:grid-cols-3"
+//                       : "md:grid-cols-3 lg:grid-cols-4"
+//                   } gap-4`}
+//                 >
+//                   {filteredCategory.images.map((media, index) => {
+//                     const mediaSrc = media.src.asset.url;
+
+//                     if (mediaSrc.endsWith(".mp4")) {
+//                       return <VideoThumbnail key={index} videoSrc={mediaSrc} />;
+//                     } else {
+//                       return (
+//                         <AnimatedImage
+//                           key={index}
+//                           image={mediaSrc}
+//                           alt={`${filteredCategory.category} Image ${index + 1}`}
+//                         />
+//                       );
+//                     }
+//                   })}
+//                 </div>
+//               </div>
+//             ))
+//         )}
+//       </div>
+//     </main>
+//   );
+// };
+
+// // Animated Image Component
+// type AnimatedImageProps = {
+//   image: string;
+//   alt: string;
+// };
+
+// const AnimatedImage = ({ image, alt }: AnimatedImageProps) => {
+//   const { ref, isInView } = useInView(0.2);
+
+//   return (
+//     <div
+//       ref={ref}
+//       className={`relative w-full h-full overflow-hidden transition-all duration-700 ease-out transform ${
+//         isInView
+//           ? "opacity-100 scale-100 rotate-0"
+//           : "opacity-0 scale-90 rotate-3"
+//       }`}
+//     >
+//       <Image
+//         src={image}
+//         alt={alt}
+//         priority
+//         layout="responsive"
+//         width={200}
+//         height={200}
+//         className="object-cover"
+//       />
+//     </div>
+//   );
+// };
+
+// // Video Thumbnail Component
+// type VideoThumbnailProps = {
+//   videoSrc: string;
+// };
+
+// const VideoThumbnail = ({ videoSrc }: VideoThumbnailProps) => {
+//   const { ref, isInView } = useInView(0.2);
+//   const [isPlaying, setIsPlaying] = useState(false);
+
+//   return (
+//     <div
+//       ref={ref}
+//       className={`relative w-full h-full overflow-hidden transition-all duration-700 ease-out transform  rounded-xl ${
+//         isInView
+//           ? "opacity-100 scale-100 rotate-0"
+//           : "opacity-0 scale-90 rotate-3"
+//       }`}
+//     >
+//       {!isPlaying ? (
+//         <div className="cursor-pointer " onClick={() => setIsPlaying(true)}>
+//           <video
+//             src={videoSrc}
+//             className="w-full h-[160px] md:h-[200px] object-cover "
+//             controls={false}
+//             muted
+//           />
+//           <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+//             <button className="px-4 py-4 bg-black/80 text-white rounded-full">
+//               <FaPlay size={16} />
+//             </button>
+//           </div>
+//         </div>
+//       ) : (
+//         <ReactPlayer
+//           url={videoSrc}
+//           playing
+//           controls
+//           width="100%"
+//           height="100%"
+//         />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default GalleryPage;
+
+
+
+
+
+
+'use client'
+
+
+import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
-import React, { useState, useEffect } from "react";
-import ReactPlayer from "react-player";
 import Image from "next/image";
+import ReactPlayer from "react-player";
 import galleryBg from "@/public/images/gallery-bg.png";
 import PagesHero from "@/app/components/PagesHero";
 import Heading from "@/app/components/Heading";
@@ -13,6 +204,7 @@ import { Gallery } from "@/app/types/gallery";
 import useInView from "@/app/hooks/useInView";
 import GallerySkeleton from "./GallerySkeleton";
 import { FaPlay } from "react-icons/fa";
+import MediaModal from "./MediaModal";
 
 const GalleryPage = () => {
   const pathname = usePathname();
@@ -20,9 +212,9 @@ const GalleryPage = () => {
   const [selectedCategory, setSelectedCategory] = useState("event");
   const [galleryImages, setGalleryImages] = useState<Gallery[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [selectedMedia, setSelectedMedia] = useState<any | null>(null);
 
   useEffect(() => {
-    // Fetch gallery data with caching
     const fetchData = async () => {
       try {
         const data = await getGalleryData();
@@ -30,11 +222,15 @@ const GalleryPage = () => {
       } catch (error) {
         console.error("Error fetching gallery data:", error);
       } finally {
-        setIsLoading(false); // Stop loading after data fetch
+        setIsLoading(false);
       }
     };
     fetchData();
   }, []);
+
+  const openModal = (media: any) => {
+    setSelectedMedia(media);
+  };
 
   return (
     <main>
@@ -83,15 +279,25 @@ const GalleryPage = () => {
                 >
                   {filteredCategory.images.map((media, index) => {
                     const mediaSrc = media.src.asset.url;
+                    const mediaDescription = media.description || "No description available";
 
                     if (mediaSrc.endsWith(".mp4")) {
-                      return <VideoThumbnail key={index} videoSrc={mediaSrc} />;
+                      return (
+                        <VideoThumbnail
+                          key={index}
+                          videoSrc={mediaSrc}
+                          description={mediaDescription}
+                          openModal={openModal}
+                        />
+                      );
                     } else {
                       return (
                         <AnimatedImage
                           key={index}
                           image={mediaSrc}
                           alt={`${filteredCategory.category} Image ${index + 1}`}
+                          description={mediaDescription}
+                          openModal={openModal}
                         />
                       );
                     }
@@ -100,28 +306,33 @@ const GalleryPage = () => {
               </div>
             ))
         )}
+
+        {/* Display modal when selected media is not null */}
+        {selectedMedia && <MediaModal media={selectedMedia} closeModal={() => setSelectedMedia(null)} />}
       </div>
     </main>
   );
 };
 
+// Updated Animated Image Component
 // Animated Image Component
 type AnimatedImageProps = {
   image: string;
   alt: string;
+  description: string;
+  openModal: (media: any) => void;
 };
 
-const AnimatedImage = ({ image, alt }: AnimatedImageProps) => {
+const AnimatedImage = ({ image, alt, description, openModal }: AnimatedImageProps) => {
   const { ref, isInView } = useInView(0.2);
 
   return (
     <div
       ref={ref}
       className={`relative w-full h-full overflow-hidden transition-all duration-700 ease-out transform ${
-        isInView
-          ? "opacity-100 scale-100 rotate-0"
-          : "opacity-0 scale-90 rotate-3"
+        isInView ? "opacity-100 scale-100 rotate-0" : "opacity-0 scale-90 rotate-3"
       }`}
+      onClick={() => openModal({ type: "image", src: image, description })}
     >
       <Image
         src={image}
@@ -132,6 +343,7 @@ const AnimatedImage = ({ image, alt }: AnimatedImageProps) => {
         height={200}
         className="object-cover"
       />
+      {/* <p className="absolute bottom-4 left-4 text-white">{description}</p> */}
     </div>
   );
 };
@@ -139,26 +351,27 @@ const AnimatedImage = ({ image, alt }: AnimatedImageProps) => {
 // Video Thumbnail Component
 type VideoThumbnailProps = {
   videoSrc: string;
+  description: string;
+  openModal: (media: any) => void;
 };
 
-const VideoThumbnail = ({ videoSrc }: VideoThumbnailProps) => {
+const VideoThumbnail = ({ videoSrc, description, openModal }: VideoThumbnailProps) => {
   const { ref, isInView } = useInView(0.2);
   const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div
       ref={ref}
-      className={`relative w-full h-full overflow-hidden transition-all duration-700 ease-out transform  rounded-xl ${
-        isInView
-          ? "opacity-100 scale-100 rotate-0"
-          : "opacity-0 scale-90 rotate-3"
+      className={`relative w-full h-full overflow-hidden transition-all duration-700 ease-out transform rounded-xl ${
+        isInView ? "opacity-100 scale-100 rotate-0" : "opacity-0 scale-90 rotate-3"
       }`}
+      onClick={() => openModal({ type: "video", src: videoSrc, description })}
     >
       {!isPlaying ? (
-        <div className="cursor-pointer " onClick={() => setIsPlaying(true)}>
+        <div className="cursor-pointer">
           <video
             src={videoSrc}
-            className="w-full h-[160px] md:h-[200px] object-cover "
+            className="w-full h-[160px] md:h-[200px] object-cover"
             controls={false}
             muted
           />
@@ -169,16 +382,12 @@ const VideoThumbnail = ({ videoSrc }: VideoThumbnailProps) => {
           </div>
         </div>
       ) : (
-        <ReactPlayer
-          url={videoSrc}
-          playing
-          controls
-          width="100%"
-          height="100%"
-        />
+        <ReactPlayer url={videoSrc} playing controls width="100%" height="100%" />
       )}
+      {/* <p className="absolute bottom-4 left-4 text-white">{description}</p> */}
     </div>
   );
 };
+
 
 export default GalleryPage;
