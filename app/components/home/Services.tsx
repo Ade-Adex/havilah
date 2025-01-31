94e0b0a69706994b084a5a1c6319bd4e957a64a7
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2025-01-31 06:29:12 +0100

    Update Services.tsx

--- app/components/home/Services.tsx
+++ app/components/home/Services.tsx
@@ -31,7 +31,7 @@ const Services = () => {
   </div>
 
   <div className="w-full flex justify-center">
-    <div className="w-full flex justify-center gap-2">
+    <div className="w-full flex ml-[30%] md:ml-[unset] md:justify-center gap-2">
       <div className="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12">
         <Image src={parking} alt="Parking Available" />
       </div>
