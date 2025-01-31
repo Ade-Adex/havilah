c04d64b8af00ad7877b3d29c9d9cc4412fa3affb
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2025-01-31 06:28:10 +0100

    Update Services.tsx

--- app/components/home/Services.tsx
+++ app/components/home/Services.tsx
@@ -9,7 +9,7 @@ const Services = () => {
     <div className=" py-12 bg-havilah-deep-cove ">
       <div className="flex flex-col md:flex-row items-center lg:justify-around gap-5 px-8 lg:px-0 text-white">
   <div className="w-full flex justify-center">
-    <div className="w-full flex justify-center gap-2">
+    <div className="w-full flex ml-[30%] md:ml-[unset] md:justify-center gap-2">
       <div className="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12">
         <Image src={modern} alt="Ultra Modern Facilities" />
       </div>
