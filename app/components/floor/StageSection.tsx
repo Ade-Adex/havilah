100737f1a2fd9a044705198b569dfa650b021771
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2025-01-31 06:04:26 +0100

    Update StageSection.tsx

--- app/components/floor/StageSection.tsx
+++ app/components/floor/StageSection.tsx
@@ -9,7 +9,7 @@ const StageSection = () => {
     <div className="py-8 px-4 md:px-12 lg:px-16 xl:px-24">
       <Heading text="Stage" className="" />
       <Line ClassName="justify-center" />
-      <div className="relative w-full h-0 pb-[26.25%] pt-16 md:pt-24"> {/* Aspect Ratio Container */}
+      <div className="relative w-full h-0 pb-[15%] lg:pb-[26.25%] pt-8 lg:pt-24"> {/* Aspect Ratio Container */}
         <Image
           src={stageImage}
           alt="stage"
