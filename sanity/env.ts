56ea27b471be98a1cf3f0716169c5bbfc43dc860
AdeAdex <adeoluamole@gmail.com>
2025-02-18 21:06:08 +0100

    updates to the navbar and env

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,13 +1,8 @@
 //  /sanity/env.ts
 
 
-// console.log('Sanity API Version:', process.env.NEXT_PUBLIC_SANITY_API_VERSION);
-// console.log('Sanity Project ID:', process.env.NEXT_PUBLIC_SANITY_PROJECT_ID);
-// console.log('Sanity Dataset:', process.env.NEXT_PUBLIC_SANITY_DATASET);
-
-
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-01-25'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION /* || '2025-01-25' */ || '2025-02-18'
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
